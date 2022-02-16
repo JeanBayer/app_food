f2583a98c159c60de5692e8a3c405e27ea87653e
Jean Bayer <jhanbayer@gmail.com>
2022-02-16 12:23:53 -0300

    sorting of folders

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   const CategoryMealsScreen();
   final routeName = '/category-meals';
@@ -10,12 +12,17 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryId = routerArgs['id'];
     final categoryTitle = routerArgs['title'] as String;
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
-      appBar: AppBar(title: Text(categoryTitle)),
-      body: Container(
-        child: const Text("Hello"),
-      ),
-    );
+        appBar: AppBar(title: Text(categoryTitle)),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(categoryMeals[index].title);
+          },
+          itemCount: categoryMeals.length,
+        ));
   }
 }
